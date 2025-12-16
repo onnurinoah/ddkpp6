4b31d3cce2be286bfe33ef28138419839e441c3e
onnurinoah <ixtys0704@gmail.com>
2025-12-16 20:59:38 +0900

    Update firebaseconfig.js

--- js/firebaseconfig.js
+++ js/firebaseconfig.js
@@ -5,14 +5,3 @@ const firebaseConfig = {
   authDomain: "ddkpp6.firebaseapp.com",
   databaseURL: "https://ddkpp6-default-rtdb.firebaseio.com",
   projectId: "ddkpp6",
-  appId: "1:838759220678:web:4b3c020482c374b2961ec7"
-};
-
-// Firebase 초기화
-// (HTML에서 로드한 firebase 객체를 사용합니다)
-if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig);
-}
-
-const db = firebase.database();
-const keywordsRef = db.ref('keywords');
